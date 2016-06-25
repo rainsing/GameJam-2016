478b2f9fab4d8ca2c1aacaeb5cc98756610ab623
Rainsing Tsai <rainsing.tsai@gmail.com>
2016-06-25 13:16:36 +0800

    Characters added to the grey box level.

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour 
+{
+	void Update ()
+	{
+		this.transform.LookAt (
+			transform.position + Camera.main.transform.rotation * Vector3.forward, 
+			Camera.main.transform.rotation * Vector3.up
+		);
+	}
+}
