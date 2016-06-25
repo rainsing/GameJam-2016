e276d61249d2d2000f89791e8e2bd6d3fe1a97bb
chenyuan <chenyuanzhang@hotmail.com>
2016-06-25 11:41:54 +0800

    上传Global.cs

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+
+	static public int Level{ get; set;}
+	static public float TimeBar{ get; set;}
+	static public int Score{ get; set;}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/temp_zcy/GlobalTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Global.Level = 100;
+		Debug.Log ("Level:" + Global.Level);
+
+		Global.TimeBar = 5.6f;
+		Debug.Log ("TimeBar:" + Global.TimeBar);
+
+		Global.Score = 500;
+		Debug.Log ("Score:" + Global.Score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
