74e97e70146ec4be07c7793594a7d1412fe9acb7
Rainsing Tsai <rainsing.tsai@gmail.com>
2016-06-25 14:14:39 +0800

    Moving characters.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,5 +9,7 @@ public class Character : MonoBehaviour
 			transform.position + Camera.main.transform.rotation * Vector3.forward, 
 			Camera.main.transform.rotation * Vector3.up
 		);
+
+		this.transform.position += new Vector3 (-2.0f * Time.deltaTime, 0.0f, 0.0f);
 	}
 }
